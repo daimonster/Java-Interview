2c5216329acf17f5cd3276c53f9a9afc34e1c26e
daimonster <546508148@qq.com>
2018-06-16 23:32:43 +0800

    a new question

---
+++ J35InversePairs.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public int InversePairs(int [] array) {
+        if(array == null || array.length == 0) return 0;
+        int [] copy = new int(array.length);
+        System.arrayco
+    }
+}
