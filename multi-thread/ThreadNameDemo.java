94fbb4c7a18afa98bc265a15146dbb2f8f8695d5
daimonster <546508148@qq.com>
2018-07-14 19:48:53 +0800

    添加thread功能

---
+++ multi-thread/ThreadNameDemo.java
@@ -0,0 +1,22 @@
+/**
+ * 取得和设置线程名称
+ * 在Thread类中可以通过getName()方法取得线程的名称，还可以通过setName()方法设置线程
+ * 的名称
+ * 线程的名称一般在启动前设置，但页允许为已经运行的线程设置名称。允许两个Tread对象有相同的名称。
+ */
+package bin.demo09.threadname;
+
+class MyThread implements Runnable{
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(Thread.currentThread().getName() + 
+            "运行， i =" +  i);
+        }
+    }
+};
+
+public class ThreadNameDemo{
+    public static void main(String[] args) {
+        MyThread my = new MyThread();
+    }
+}
