936bc8effa600a182d7ca22c3ef63112ca9bb688
daimonster <546508148@qq.com>
2018-07-16 15:32:26 +0800

    完成一道题目

--- NewCoder/test.java
+++ NewCoder/test.java
@@ -1,33 +1,9 @@
+package bin.test;
 import java.util.*;
 public class test {
- 
-    static void showpush(Stack<Integer> st, int a) {
-        st.push(new Integer(a));
-        System.out.println("push(" + a + ")");
-        System.out.println("stack: " + st);
+    int a[] = {4,2,3,1,6};
+    for(int x : a){
+        System.out.println(x);
     }
- 
-    static void showpop(Stack<Integer> st) {
-        System.out.print("pop -> ");
-        Integer a = (Integer) st.pop();
-        System.out.println(a);
-        System.out.println("stack: " + st);
-    }
- 
-    public static void main(String args[]) {
-        Stack<ListNode> st = new Stack<ListNode>();
-        
-        System.out.println("stack: " + st);
-        showpush(st, 42);
-        showpush(st, 66);
-        showpush(st, 99);
-        showpop(st);
-        showpop(st);
-        showpop(st);
-        try {
-            showpop(st);
-        } catch (EmptyStackException e) {
-            System.out.println("empty stack");
-        }
-    }
+    
 }
